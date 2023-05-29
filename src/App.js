e936fc33435ccd94fa55aab699bf2f2f8df37cff
Jamie Crane <117677398+cjamierun@users.noreply.github.com>
2023-05-29 13:03:27 +0000

    Update App.js
    Preparing to test tooltip text formatting options

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Tooltip text="Sample tooltip text.">
+      <Tooltip text="Sample tooltip text. But what happens when the message goes very long? Is there a limit?">
         <h2>Start editing to see some magic happen!</h2>
       </Tooltip>
       <h2>The tooltip should cover this text.</h2>
